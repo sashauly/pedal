817fd28eeff2c8b2dfed7fb4e37d0f5c5e599ce8
sashauly <77851963+sashauly@users.noreply.github.com>
2025-06-17 18:42:18 +0300

    setup vite-plugin-pwa with assets-generator

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,90 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const BASE_PATH = process.env.PAGES_BASE_PATH ?? "/cycle-tracker";
+
+const isProd = process.env.NODE_ENV === "production";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: isProd ? BASE_PATH : "",
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.tile\.openstreetmap\.org\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "openstreetmap-tiles",
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+              // cacheKeyWillBeUsed: async ({ request }) => {
+              //   return `${request.url}`;
+              // }
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon-180x180.png",
+        "bicycle.svg",
+      ],
+      manifest: {
+        name: "Педаль Велосипедиста",
+        short_name: "Педаль",
+        description: "Приложение для отслеживания велосипедных маршрутов",
+        theme_color: "#3B82F6",
+        background_color: "#1F2937",
+        display: "standalone",
+        orientation: "portrait",
+        display_override: ["window-controls-overlay", "standalone", "browser"],
+        lang: "ru",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
 });
